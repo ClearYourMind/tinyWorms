eccee2a6b3732eac91845c4ff91b9702a055a3b9
PyDevill <delphadyth@gmail.com>
2025-09-21 22:28:47 +0700

    Added Player class. Modified terrain drawing code. Made checking cells for terrain bits

--- worm_anim.ino
+++ worm_anim.ino
@@ -3,43 +3,119 @@
 Arduboy2 arduboy;
 Sprites sprites;
 
-#define MAX_LINES 24
+//#include "FixedMath.c"   // not using fmath by this time
+
+#define MAX_LINES 16
+
+uint16_t counter = 0;
 
 uint8_t const ground[] PROGMEM = {
   7, 8, 
   0x1C, 0x3E, 0x7F, 0x7F, 0x7F, 0x3E, 0x1C
 };
 uint32_t field[MAX_LINES] = {
-  0xFFFFFFFF,
-  0x00FFFFFF,
-  0x0001FF1F,
-  0xE0003003,
+  0x00000000,
+  0x00000000,
+  0x00000000,
+  0x00000000,
 
-  0xC0000001,
-  0x00C00007,
-  0x03F0003F,
-  0x01E0380F,
+  0x000C0000,
+  0x003F0000,
+  0x007FC000,
+  0x00FFE000,
 
-  0x0001FE07,
-  0x0007FF06,
-  0xC00FFF0D,
-  0xF00FFE0E,
+  0x01FFF000,
+  0x03FFFC00,
+  0x07F3FE00,
+  0x0FC0FF03,
 
-  0x3F03E01A,
-  0xCF80001D,
-  0x31C00035,
-  0x0EE0007B
+  0x1F003FFF,
+  0xFC000FFF,
+  0xF80000FF,
+  0xE0000000
 };
+
+class Player {
+  public:
+    bool landed;
+    int8_t dir; // -1 .. 1
+
+    int32_t x;
+    int32_t y;
+    int32_t dx;
+    int32_t dy;
+    int32_t walk_speed;
+    int8_t* cx;
+    int8_t* cy;
+    Player();
+    ~Player();
+    void draw();
+    void process();
+};
+
+Player::Player() {
+  walk_speed = 1;
+  cx = new int8_t[3];
+  cy = new int8_t[3];
+};
+
+Player::~Player() {
+  delete [] cx;
+  delete [] cy;
+};
+
+void Player::draw() {
+  int8_t osc = counter % 2;
+  arduboy.drawFastVLine(x-1, y, 16, osc);
+  arduboy.drawFastVLine(x, y, 16, osc);
+  arduboy.drawFastVLine(x+1, y, 16, osc);
+
+  arduboy.drawRect(cx[0] << 2, cy[0] << 2, 7, 7, osc);
+  arduboy.drawRect(cx[1] << 2, cy[1] << 2, 7, 7, osc);
+};
+
+void Player::process() {
+  int32_t cell;
+  cx[0] = (x - 2) >> 2;
+  cy[0] = (y + 16) >> 2;
+  cx[1] = (x + 2) >> 2;
+  cy[1] = (y + 16) >> 2;
+
+  if (arduboy.pressed(UP_BUTTON)) {
+    y = max(0, y - walk_speed);
+  };
+  if (arduboy.pressed(DOWN_BUTTON)) {
+    y = min(HEIGHT, y + walk_speed);
+  };
+  if (arduboy.pressed(LEFT_BUTTON)) {
+    x = max(0, x - walk_speed);
+  };
+  if (arduboy.pressed(RIGHT_BUTTON)) {
+    x = min(WIDTH, x + walk_speed);
+  };
+
+  if (arduboy.pressed(A_BUTTON)) {
+    field[cy[0]] = field[cy[0]] | (0x80000000 >> cx[0]);
+  };
+  cell = (field[cy[0]] & (0x80000000 >> cx[0])) > 0 ? 1 : 0;
+  arduboy.setCursor(0, 0);
+  arduboy.print(cell);
+  cell = (field[cy[1]] & (0x80000000 >> cx[1])) > 0 ? 1 : 0;
+  arduboy.setCursor(8, 0);
+  arduboy.print(cell);
+};
+
+Player player;
 
 void setup() {
   arduboy.boot();
   arduboy.flashlight();
   arduboy.systemButtons();
   arduboy.setFrameRate(30);
-  //arduboy.initRandomSeed();
+  player.x = 5;
+  player.y = 5;
 }
 
-uint16_t counter = 0;
 uint8_t yy = 0;
 uint8_t xx = 0;
 uint8_t line_count = 1;
@@ -51,20 +127,21 @@ void loop() {
   arduboy.pollButtons();
 
   arduboy.fillScreen(WHITE);
-  // counter++;
-  // if ((counter % MAX_LINES) == 0)
-  //   line_count = (line_count + 1) % MAX_LINES;
+  counter++;
 
   for (uint8_t j=0; j<MAX_LINES; j++) {
     yy = j << 2;
     for (uint8_t i=0; i<32; i++)
-    if ((field[j] >> i & 1) == 1) {
-      xx = (30-i) << 2;
+    if ((field[j] & (0x80000000 >> i)) > 0) {
+      xx = i << 2;
       sprites.drawErase(xx, yy, ground, 0);
     };
   };
 
+  player.process();
+  if (counter % 2 == 0)
+    player.draw();
+
   arduboy.display();
 
-//  test_animation();
 }
