abce77ac111d87e7f60ac03677420c0cfe553503
PyDevill <delphadyth@gmail.com>
2026-01-17 21:15:20 +0700

    Shrinked fixedmath.h module, uses less ROM, faster. FBITS = 6, one bit is now used for sign

--- fixedmath.h
+++ fixedmath.h
@@ -3,48 +3,31 @@
 
 #include <stdint.h>
 #include <avr/pgmspace.h>
-#define SCPREC 8                  // sectors in quadrant (precision)
-#define SCPREC_LOG2 3             // power of 2 sectors in a quadrant (2^3 = 8)
 #define MAXANGLESEC 32 //SCPREC * 4
-#define FBITS 7
+#define FBITS 6
 
 //// Fixed point math //////
 inline int32_t fmul(int32_t A, int32_t B) {
 	return (A * B) >> FBITS;
 }
 
-inline int16_t ftoint(int32_t A) {
-  return A >> FBITS;
-}
 ////////////////////////////
 
 ////   Sin/Cos table  //////
-const uint8_t scTab[SCPREC + 1] PROGMEM = {
-//  0, 50 >> 4, 98 >> 4, 142 >> 4, 180 >> 4, 212 >> 4, 236 >> 4, 250 >> 4, 256 >> 4
-  0, 25, 49, 71, 90, 106, 118, 125, 128
+const int8_t sTab[MAXANGLESEC] PROGMEM = {
+  // 0,  25,  49,  71,  90,  106,  118,  125,  128,  125,  118,  106,  90,  71,  49,  25,
+  // 0, -25, -49, -71, -90, -106, -118, -125, -127, -125, -118, -106, -90, -71, -49, -25
+  0,  12,  24,  35,  45,  53,  59,  62,  64,  62,  59,  53,  45,  35,  24,  12,
+  0, -12, -24, -35, -45, -53, -59, -62, -64, -62, -59, -53, -45, -35, -24, -12
+};
+const int8_t cTab[MAXANGLESEC] PROGMEM = {
+   64,  62,  59,  53,  45,  35,  24,  12, 0, -12, -24, -35, -45, -53, -59, -62,
+  -64, -62, -59, -53, -45, -35, -24, -12, 0,  12,  24,  35,  45,  53,  59,  62
 };
 
-inline void getSinCos(uint8_t sec, int16_t *s, int16_t *c) {   // a - angle. must be + and <=360
-  uint8_t q = sec >> SCPREC_LOG2;
-
-  switch (q) {
-  case 0:
-    *s = pgm_read_byte_near(scTab + sec % SCPREC);
-    *c = pgm_read_byte_near(scTab + SCPREC - sec % SCPREC);
-  break;
-  case 1:
-    *s =  pgm_read_byte_near(scTab + SCPREC - sec % SCPREC);
-    *c = -pgm_read_byte_near(scTab + sec % SCPREC);
-  break;
-  case 2:
-    *s = -pgm_read_byte_near(scTab + sec % SCPREC);
-    *c = -pgm_read_byte_near(scTab + SCPREC - sec % SCPREC);
-  break;
-  case 3:
-    *s = -pgm_read_byte_near(scTab + SCPREC - sec % SCPREC);
-    *c =  pgm_read_byte_near(scTab + sec % SCPREC);
-  break;
-  };
+inline void getSinCos(uint8_t sec, int16_t *s, int16_t *c) {  // sec must be <MAXANGLESEC
+  *s = (int8_t)pgm_read_byte_near(sTab + sec);
+  *c = (int8_t)pgm_read_byte_near(cTab + sec);
 }
 ////////////////////////////
 
